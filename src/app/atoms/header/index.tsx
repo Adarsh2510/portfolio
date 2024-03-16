f5a21e5f2a1e6ccb7f747deef290c515039b5687
Adarsh2510 <adarsh25trivedi@gmail.com>
2024-03-16 23:18:29 +0530

    feat: initial setup

---
+++ src/app/atoms/header/index.tsx
@@ -0,0 +1,9 @@
+import { css } from '/styled-system/css'
+
+const Header = () => {
+    return (
+<></>
+    );
+};
+
+export default Header;
---
+++ src/app/atoms/header/styles.tsx
@@ -0,0 +1,6 @@
+import { css } from "styled-system/css";
+
+export const headerContainer = css({
+    
+
+});

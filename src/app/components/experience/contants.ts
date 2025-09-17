41c7202a8693fee1d4b472fdcee4a13bcf4efa24
adarsh2510 <adarsh25trivedi@gmail.com>
2025-09-17 21:37:37 +0530

    chore: update experience details and enhance highlights

--- src/app/components/experience/contants.ts
+++ src/app/components/experience/contants.ts
@@ -5,11 +5,14 @@ export const EXPERIENCES_DATA = [
     {
         SVGElement : HeadoutLogo,
         url: "https://in.linkedin.com/in/adarsh2510",
-        role: "Headout, Inc - SET",
+        role: "Headout, Inc - Software Engineer",
         date: "Nov 2022, Present",
         experienceHighlights: [
-            "Developed automation scripts in Selenium python for partner integrations",
-            "Develpoed test automation scripts for Microbrands to reduce the manual testing efforts"
+            "Developed reusable UI components for a travel booking platform serving 1.96M+ monthly visitors, including product cards, banners and user interface elements that enhanced customer engagement across 250+ global destinations",
+            "Implemented automated visual testing tools for Next.js 14 migration, reducing manual testing efforts by 85%",
+            "Optimized API performance by leveraging projections and indexes, resulting in a ~90% reduction in critical components like search bar load times",
+            "Collaborated in a fast-paced startup environment, taking on multiple roles and responsibilities as needed, to lead the migration from Prismic to Payload CMS and Next.js 14",
+            "Engineered automation scripts using Selenium (Python) to streamline and enhance partner integration processes",
         ]
     },
     {
@@ -18,8 +21,8 @@ export const EXPERIENCES_DATA = [
         role: "Capgemini <-> Morgan Stanley - SWE",
         date: "July 2021 - Nov 2022",
         experienceHighlights: [
-            "Developed Automation scripts for Morgan Stanley account",
-            "Experienced in technologies like Java, Spring Boot, Selenium, Cucumber, Rest Assured etc"
+            "Authored automation scripts for the Morgan Stanley account, enhancing operational efficiency",
+            "Proficient in Java, Spring Boot, Selenium, Cucumber, and Rest Assured, applied in various projects"
         ]
     },
     {
@@ -28,8 +31,8 @@ export const EXPERIENCES_DATA = [
         role: "Capgemini - Intern",
         date: "March 2021 - June 2022",
         experienceHighlights: [
-            "Trained in Java, Gradle, Spring Boot, Oracle SQL, Angular, Jenkins, Git",
-            "Developed Tata Sky portal clone with functionalities like creating recharges, accounts, packs, Admin portal, etc"
+            "Completed training in Java, Gradle, Spring Boot, Oracle SQL, Angular, Jenkins, and Git, enhancing technical proficiency",
+            "Engineered a Tata Sky portal clone, incorporating features such as recharge creation, account management, package selection, and an admin portal"
         ]
     },
 ]
